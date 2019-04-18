2882197f9e007ce30058b05afa4adeed8e578c42
dzimiks <vana997@gmail.com>
2019-04-18 13:58:44 +0200

    Init project

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+/**
+ * @author dzimiks
+ * Date: 18-04-2019 at 13:57
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("File Storage");
+	}
+}
