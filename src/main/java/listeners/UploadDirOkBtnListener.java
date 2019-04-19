5b3f892c79175a11b70243ae6fd841ed0f6d2964
dzimiks <vana997@gmail.com>
2019-04-19 08:26:03 +0200

    Add upload

--- src/main/java/listeners/UploadDirOkBtnListener.java
+++ src/main/java/listeners/UploadDirOkBtnListener.java
@@ -2,6 +2,7 @@ package listeners;
 
 import dialogs.DownloadDirDialog;
 import dialogs.UploadDirDialog;
+import dropbox.models.DropboxDirectory;
 import model.Student;
 import models.LocalDirectory;
 
@@ -10,41 +11,45 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class UploadDirOkBtnListener implements ActionListener {
-    private UploadDirDialog dialog;
-    private Student student;
+	private UploadDirDialog dialog;
+	private Student student;
 
-    public UploadDirOkBtnListener(UploadDirDialog dialog, Student student) {
-        this.dialog = dialog;
-        this.student = student;
-    }
+	public UploadDirOkBtnListener(UploadDirDialog dialog, Student student) {
+		this.dialog = dialog;
+		this.student = student;
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        String src = dialog.getTfDirSource();
-        String dest = dialog.getTfDirDestination();
-        String implementation = student.getImplementation();
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String src = dialog.getTfDirSource();
+		String dest = dialog.getTfDirDestination();
+		String implementation = student.getImplementation();
+		String accessToken = student.getAccessToken();
 
-        if (implementation.equals("local")) {
-            try {
-                LocalDirectory ld = new LocalDirectory();
-                // TODO: Handle exceptions if empty string etc.
+		if (implementation.equals("local")) {
+			try {
+				LocalDirectory ld = new LocalDirectory();
+				// TODO: Handle exceptions if empty string etc.
 //                if (name.equals("")) {
 //                    name = "new-local-directory";
 //                }
 //
 //                ld.create(name, path);
-                ld.upload(src,dest);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+				ld.upload(src, dest);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
 
-            System.out.println("=== Create directory ===");
-            System.out.println("Src: " + src);
-            System.out.println("Dest: " + dest);
-        } else {
-            System.out.println("Error");
-        }
+			System.out.println("=== Create directory ===");
+			System.out.println("Src: " + src);
+			System.out.println("Dest: " + dest);
+		} else if (implementation.equals("dropbox")) {
+			DropboxDirectory dropboxDirectory = new DropboxDirectory(accessToken);
+			dropboxDirectory.upload(src, dest);
+		} else {
+			System.out.println("Error");
+		}
 
-        this.dialog.setVisible(false);
-    }
+		this.dialog.setVisible(false);
+	}
 }
--- src/main/java/listeners/UploadFileOkBtnListener.java
+++ src/main/java/listeners/UploadFileOkBtnListener.java
@@ -2,6 +2,8 @@ package listeners;
 
 import dialogs.UploadDirDialog;
 import dialogs.UploadFileDialog;
+import dropbox.models.DropboxDirectory;
+import dropbox.models.DropboxFile;
 import model.Student;
 import models.LocalDirectory;
 import models.LocalFile;
@@ -10,41 +12,46 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class UploadFileOkBtnListener implements ActionListener {
-    private UploadFileDialog dialog;
-    private Student student;
+	private UploadFileDialog dialog;
+	private Student student;
 
-    public UploadFileOkBtnListener(UploadFileDialog dialog, Student student) {
-        this.dialog = dialog;
-        this.student = student;
-    }
+	public UploadFileOkBtnListener(UploadFileDialog dialog, Student student) {
+		this.dialog = dialog;
+		this.student = student;
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        String src = dialog.getTfDirSource();
-        String dest = dialog.getTfDirDestination();
-        String implementation = student.getImplementation();
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String src = dialog.getTfDirSource();
+		String dest = dialog.getTfDirDestination();
+		String implementation = student.getImplementation();
+		String accessToken = student.getAccessToken();
 
-        if (implementation.equals("local")) {
-            try {
-                LocalFile lf = new LocalFile();
-                // TODO: Handle exceptions if empty string etc.
+		if (implementation.equals("local")) {
+			try {
+				LocalFile lf = new LocalFile();
+				// TODO: Handle exceptions if empty string etc.
 //                if (name.equals("")) {
 //                    name = "new-local-directory";
 //                }
 //
 //                ld.create(name, path);
-                lf.upload(src,dest);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+				lf.upload(src, dest);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
 
-            System.out.println("=== Create directory ===");
-            System.out.println("Src: " + src);
-            System.out.println("Dest: " + dest);
-        } else {
-            System.out.println("Error");
-        }
+			System.out.println("=== Create directory ===");
+			System.out.println("Src: " + src);
+			System.out.println("Dest: " + dest);
+		} else if (implementation.equals("dropbox")) {
+			DropboxDirectory dropboxDirectory = new DropboxDirectory(accessToken);
+			DropboxFile dropboxFile = new DropboxFile(dropboxDirectory.getClient());
+			dropboxFile.upload(src, dest);
+		} else {
+			System.out.println("Error");
+		}
 
-        this.dialog.setVisible(false);
-    }
+		this.dialog.setVisible(false);
+	}
 }
