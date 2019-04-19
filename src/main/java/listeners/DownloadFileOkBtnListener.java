68ea7e7769009fc90e84c22afed961c68a0b30e6
dzimiks <vana997@gmail.com>
2019-04-19 06:44:53 +0200

    Add download file dropbox

--- src/main/java/listeners/DownloadFileOkBtnListener.java
+++ src/main/java/listeners/DownloadFileOkBtnListener.java
@@ -1,50 +1,55 @@
 package listeners;
 
-import dialogs.DownloadDirDialog;
 import dialogs.DownloadFileDialog;
+import dropbox.models.DropboxDirectory;
+import dropbox.models.DropboxFile;
 import model.Student;
-import models.LocalDirectory;
 import models.LocalFile;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class DownloadFileOkBtnListener implements ActionListener {
-    private DownloadFileDialog dialog;
-    private Student student;
+	private DownloadFileDialog dialog;
+	private Student student;
 
-    public DownloadFileOkBtnListener(DownloadFileDialog dialog, Student student) {
-        this.dialog = dialog;
-        this.student = student;
-    }
+	public DownloadFileOkBtnListener(DownloadFileDialog dialog, Student student) {
+		this.dialog = dialog;
+		this.student = student;
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        String src = dialog.getTfDirSource();
-        String dest = dialog.getTfDirDestination();
-        String implementation = student.getImplementation();
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String src = dialog.getTfDirSource();
+		String dest = dialog.getTfDirDestination();
+		String implementation = student.getImplementation();
+		String accessToken = student.getAccessToken();
 
-        if (implementation.equals("local")) {
-            try {
-                LocalFile lf = new LocalFile();
-                // TODO: Handle exceptions if empty string etc.
+		if (implementation.equals("local")) {
+			try {
+				LocalFile lf = new LocalFile();
+				// TODO: Handle exceptions if empty string etc.
 //                if (name.equals("")) {
 //                    name = "new-local-directory";
 //                }
 //
 //                ld.create(name, path);
-                lf.move(src,dest);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+				lf.move(src, dest);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
 
-            System.out.println("=== Create directory ===");
-            System.out.println("Src: " + src);
-            System.out.println("Dest: " + dest);
-        } else {
-            System.out.println("Error");
-        }
+			System.out.println("=== Create directory ===");
+			System.out.println("Src: " + src);
+			System.out.println("Dest: " + dest);
+		} else if (implementation.equals("dropbox")) {
+			DropboxDirectory dropboxDirectory = new DropboxDirectory(accessToken);
+			DropboxFile dropboxFile = new DropboxFile(dropboxDirectory.getClient());
+			dropboxFile.download(src, dest);
+		} else {
+			System.out.println("Error");
+		}
 
-        this.dialog.setVisible(false);
-    }
+		this.dialog.setVisible(false);
+	}
 }
